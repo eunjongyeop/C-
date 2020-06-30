5d5c01f6203cca685af6c3f6c145a9a11a76a401
eunjongyeop <48788795+eunjongyeop@users.noreply.github.com>
2020-06-30 17:24:22 +0900

    Ex07_Class

---
+++ ConsoleApp/Ex07_Class/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex07_Class
+{
+    class Cube
+    {
+        private int x, y, z;
+        public static int countOfInstance;
+
+        public Cube(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            countOfInstance++;
+        }
+
+        public int GetVoume()
+        {
+            return x*y*z;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cube cube1 = new Cube(1, 2, 3);
+            System.Console.WriteLine($"생성된 Cube의 개수 : {Cube.countOfInstance}");
+            System.Console.WriteLine($"cube1의 부피 : {cube1.GetVoume()}");
+            System.Console.WriteLine();
+
+            Cube cube2 = new Cube(3, 4, 5);
+            System.Console.WriteLine($"생성된 Cube의 개수 : {Cube.countOfInstance}");
+            System.Console.WriteLine($"cube의 부피 : {cube2.GetVoume()}");
+        }
+    }
+}
---
+++ ConsoleApp/Static/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Global
+{
+    public static int Count = 0;// 메소드나 필드가 클래스 자체에 소속되도록 하는 한정자, 프로그램전체에서 공유해야 하는 경우
+}
+
+class ClassA
+{
+    public ClassA()
+    {
+        Global.Count++;
+    }
+}
+
+class ClassB
+{
+    public ClassB()
+    {
+        Global.Count++;
+    }
+}
+
+class MainApp
+{
+    static void Main()
+    {
+        Console.WriteLine($"Global.Count : {Global.Count}");
+
+        new ClassA();
+        new ClassA();
+        new ClassB();
+        new ClassB();
+
+        Console.WriteLine($"Global.Count : {Global.Count}");
+    }
+}
