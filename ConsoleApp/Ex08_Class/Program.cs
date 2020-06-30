a37a469ffbee31acfe29b240584313e506864510
eunjongyeop <48788795+eunjongyeop@users.noreply.github.com>
2020-06-30 17:40:38 +0900

    Ex08_Class

--- ConsoleApp/Ex08_Class/Program.cs
+++ ConsoleApp/Ex08_Class/Program.cs
@@ -10,21 +10,22 @@ namespace Ex08_Class
     {
         public int a, b, c;
 
-        public Class1(): this(0)
-        {
-
-        }
-
-        public Class1(int b) : this(b, 0)
-        {
-            
-        }
-
-        public Class1(int b, int c)
+        public Class1()
         {
             this.a = 1;
+            System.Console.WriteLine("Class1() 호출");
+        }
+
+        public Class1(int b) : this()
+        {
             this.b = b;
+            System.Console.WriteLine($"Class1({b}) 호출");
+        }
+
+        public Class1(int b, int c): this(b)
+        {
             this.c = c;
+            System.Console.WriteLine($"Class1({b}, {c}) 호출");
         }
     }
 
@@ -35,17 +36,14 @@ namespace Ex08_Class
             System.Console.WriteLine("*****기본 생성자 사용*****");
 
             Class1 class1 = new Class1();
-            System.Console.WriteLine("Class1() 호출");
             System.Console.WriteLine($"a:{class1.a}, b:{class1.b}, c:{class1.c}");
             System.Console.WriteLine();
 
             Class1 class2 = new Class1(2);
-            System.Console.WriteLine("Class1(2) 호출");
             System.Console.WriteLine($"a:{class2.a}, b:{class2.b}, c:{class2.c}");
             System.Console.WriteLine();
 
             Class1 class3 = new Class1(2, 3);
-            System.Console.WriteLine("Class1(2, 3) 호출");
             System.Console.WriteLine($"a:{class3.a}, b:{class3.b}, c:{class3.c}");
             System.Console.WriteLine();
         }
