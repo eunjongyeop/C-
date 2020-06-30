5c553bb08194a4824ad7830ae606a66806e4a0d9
eunjongyeop <48788795+eunjongyeop@users.noreply.github.com>
2020-06-30 18:06:10 +0900

    AccessModifiler

--- ConsoleApp/Ex09_Class_AccessModifiler/Program.cs
+++ ConsoleApp/Ex09_Class_AccessModifiler/Program.cs
@@ -6,10 +6,48 @@ using System.Threading.Tasks;
 
 namespace Ex09_Class_AccessModifiler
 {
+    class A
+    {
+        public int value1 = 10;
+        protected int value2 = 20;
+        private int value3 = 30;
+
+        public class C : A
+        {
+            public int GetValue3()
+            {
+                return value3;
+            }
+        }
+    }
+
+    class B : A
+    {
+        public int GetValue1()
+        {
+            return value1;
+        }
+
+        public int GetValue2()
+        {
+            return value2;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            A a = new A();
+            System.Console.WriteLine($"a.value1 : {a.value1}");
+            //System.Console.WriteLine($"a.value2 : {a.value2}");
+
+            B b = new B();
+            System.Console.WriteLine($"b.value1 : {b.GetValue1()}");
+            System.Console.WriteLine($"b.value2 : {b.GetValue2()}");
+
+            var c = new A.C();
+            System.Console.WriteLine($"c.value3 : {c.GetValue3()}");
         }
     }
 }
---
+++ ConsoleApp/Ex10_Class_AccessModifiler/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex10_Class_AccessModifiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WaterHeater heater = new WaterHeater();
+            Console.WriteLine($"현재 온도는 {heater.GetTemperature()}도 입니다.");
+            Console.WriteLine(heater.SetTemperature(200));
+            Console.WriteLine(heater.SetTemperature(20));
+            Console.WriteLine($"현재 온도는 {heater.GetTemperature()}도 입니다.");
+        }
+    }
+
+    class WaterHeater
+    {
+        private int temperature;
+
+        public String SetTemperature(int temp)
+        {
+           if (temp < 0 || temp > 50)
+            {
+                return $"에러: 온도가 {temp}도로 정상 범위를 벗어남.";
+            }
+
+            temperature = temp;
+            return $"온도가 {temp}도로 설정됨.";
+        }
+
+        public int GetTemperature()
+        {
+            return temperature;
+        }
+    }
+}
